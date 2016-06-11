5b10b5b0f551cd32b754f01c06dc3724b690e30c
NejcZdovc <nejc.zdovc@3zsistemi.si>
2016-06-11 10:46:23 +0200

    Working on slimmer version for npm; Version 0.0.3

---
+++ src/ng2-select2.ts
@@ -0,0 +1,10 @@
+import { Select2Component }  from './select2/index';
+export {OptionData} from './select2/index';
+
+const SELECT2_DIRECTIVES:Array<any> = [Select2Component];
+
+export default {
+    directives: [
+        SELECT2_DIRECTIVES
+    ]
+};
