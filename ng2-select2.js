4badc6fb245f7ff7ed4508e91d5c9a2a09d8960f
NejcZdovc <nejc.zdovc@3zsistemi.si>
2016-08-14 23:49:37 +0200

    Removed naming change

--- ng2-select2.js
+++ ng2-select2.js
@@ -1,3 +1,3 @@
 "use strict";
 var select2_component_1 = require('./src/select2/select2.component');
-exports.SELECT2_DIRECTIVES = select2_component_1.Select2Component;
+exports.Select2Component = select2_component_1.Select2Component;
--- ng2-select2.ts
+++ ng2-select2.ts
@@ -1,3 +1,2 @@
 export { Select2OptionData, Select2TemplateFunction } from './src/select2/select2.interface';
-export { Select2Component as SELECT2_DIRECTIVES } from './src/select2/select2.component';
-
+export { Select2Component } from './src/select2/select2.component';
