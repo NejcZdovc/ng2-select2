b208ea9aab880c872030191ec9822485701dddc9
Terrance Woods <tjwoods58@gmail.com>
2017-03-20 14:28:58 -0400

    Generated output so it can be used via npm directly

---
+++ dist/ng2-select2.d.ts
@@ -0,0 +1,4 @@
+export { Select2OptionData, Select2TemplateFunction } from './ng2-select2.interface';
+export { Select2Component } from './ng2-select2.component';
+export declare class Select2Module {
+}
---
+++ dist/ng2-select2.interface.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="jquery" />
+/// <reference types="select2" />
+export interface Select2OptionData {
+    id: string;
+    text: string;
+    disabled?: boolean;
+    children?: Array<Select2OptionData>;
+    additional?: any;
+}
+export interface Select2TemplateFunction {
+    (state: Select2OptionData): JQuery | string;
+}
